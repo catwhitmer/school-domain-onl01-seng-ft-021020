8b3755dee1f87933b94f5bbde33dc830026676f1
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-19 18:39:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    unless roster.has_key?(grade)
+      roster[grade] = [name]
+    end
+      roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sorted_list
+    sorted_hash = {}
+    @roster.sort.each do |name, grade|
+      sorted_hash[grade] = grade.sort
+  end
+    sorted_hash
+  end
+end
